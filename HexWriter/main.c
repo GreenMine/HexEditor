c57672300671592954981999751feb18a3641f44
GreenMine <greenmine94@gmail.com>
2020-04-17 12:34:04 +0300

    Add help command and bug fix.

--- HexWriter/main.c
+++ HexWriter/main.c
@@ -1,11 +1,9 @@
-//#define __CRTDBG_MAP_ALLOC
-//#include <crtdbg.h>
-//#define DEBUG_NEW new(_NORMAL_BLOCK, __FILE__, __LINE__)
-//#define new DEBUG_NEW
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include "args.h"
+
+#define COMMAND_COUNT 4
 
 int hex_open(cmd_args_t arguments);
 
@@ -13,30 +11,37 @@ int hex_create(cmd_args_t arguments);
 
 int hex_exit();
 
+int hex_help();
+
 FILE* fp;
 
-int main() {
-	struct {
-		int command_name; 
-		int (*command_f)(cmd_args_t arguments); /*Функция обработки*/
-	} m[3] = {
-	{'o', hex_open }, {'c', hex_create }, {'e', hex_exit}};
+struct {
+	int name;
+	const char* description;
+	int (*f)(cmd_args_t arguments);
+} commands[COMMAND_COUNT] = {
+	{'o', "Opening .bin file", hex_open}, {'c', "Create new .bin file", hex_create}, {'e', "Close program", hex_exit}, {'h', "Information about commands", hex_help}
+};
 
+int main() {
 	char command_buffer[64] = {0x00};
+	bool cmd_exists;
 	while (true) {
+		cmd_exists = false;
 		printf("XHE $> ");
 		gets(command_buffer);
-		//putchar('\n');
-		for (int i = 0; i < 2; i++) {
-			if (m[i].command_name == command_buffer[0]) {
+		for (int i = 0; i < COMMAND_COUNT; i++) {
+			if (commands[i].name == command_buffer[0]) {
 				cmd_args_t cmd = to_pretty_arguments(command_buffer);
-				m[i].command_f(cmd);
+				commands[i].f(cmd);
 				free_arguments(cmd);
+				cmd_exists = true;
 				break;
 			}
 		}
+		if (!cmd_exists)
+			printf("Command not exists.\nType 'h' to get all commands\n");
 	}
-	//_CrtDumpMemoryLeaks();
 	return 0;
 }
 
@@ -44,14 +49,18 @@ int hex_open(cmd_args_t arguments) {
 	printf("Arguments(%d):\n", arguments.count);
 	for (int i = 0; i < arguments.count; i++)
 		printf("[%d] %s\n", i + 1, arguments.arguments[i]);
-	return 1;
-}
-
-int hex_create(cmd_args_t arguments) {
-	printf("Arguments for test_2: %s\n", arguments);
 	return 0;
 }
 
+int hex_create(cmd_args_t arguments) {
+	return 0;
+}
+
+int hex_help() {
+	printf("Commands(%d):\n", COMMAND_COUNT);
+	for (int i = 0; i < COMMAND_COUNT; i++)
+		printf("%c - %s\n", commands[i].name, commands[i].description);
+}
 
 int hex_exit() {
 	system("pause");
